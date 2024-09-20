a951c5bff7d35db889e85cc3f686c94306ee860e
talip efe <talipefe35@gmail.com>
2024-09-20 10:14:56 +0300

    first

---
+++ Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace Directory_02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button8 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(602, 311);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 27;
+            this.button8.Text = "GetFullPath";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(256, 164);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(179, 212);
+            this.listBox2.TabIndex = 26;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(602, 268);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(95, 23);
+            this.button7.TabIndex = 25;
+            this.button7.Text = "Selected Item";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(602, 222);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 24;
+            this.button6.Text = "GetParent()";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(602, 193);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 23;
+            this.button5.Text = "Root";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(602, 164);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(154, 23);
+            this.button4.TabIndex = 22;
+            this.button4.Text = "\"P\" names Directories";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(602, 132);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(95, 23);
+            this.button3.TabIndex = 21;
+            this.button3.Text = "GetDirectories";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(602, 103);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 20;
+            this.button2.Text = "log files";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(44, 164);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(166, 212);
+            this.listBox1.TabIndex = 19;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(602, 74);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "C:\\  files";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(474, 349);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "label4";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(474, 293);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "label3";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(474, 232);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "label2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(474, 164);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(979, 582);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Directory_02
+{
+    public partial class Form1 : Form
+    {
+        public string text1;
+        public Form1()
+        {
+            InitializeComponent();
+
+            listBox1.SelectedIndexChanged+=ListBox1_SelectedIndexChanged;
+            listBox1.SelectedValueChanged+=ListBox1_SelectedValueChanged;
+
+
+            //                    // Get the current directory.
+            //                    string path = Directory.GetCurrentDirectory();
+            //                    string target = @"c:\temp";
+            //                    Console.WriteLine("The current directory is {0}", path);
+            //
+            //                    if (!Directory.Exists(target))
+            //{
+            //    Directory.CreateDirectory(target);
+            //}
+
+            //                    // Change the current directory.
+            //                    Environment.CurrentDirectory = (target);
+            //                    if (path.Equals(Directory.GetCurrentDirectory()))
+            //                    {
+            //                        Console.WriteLine("You are in the temp directory.");
+            //                    }
+            //                    else
+            //                    {
+            //                        Console.WriteLine("You are not in the temp directory.");
+            //                    }
+            //                }
+        }
+
+        private void ListBox1_SelectedValueChanged(object sender, EventArgs e)
+        {
+            text1= listBox1.SelectedItem.ToString();
+            label4.Text = text1;
+        }
+
+        private void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //text1= listBox1.SelectedItem.ToString();
+            //label4.Text = text1;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex >= 0)
+            {
+                // string[] array2 = Directory.GetFiles(@"C:\", "*.log"); // <-- Case-insensitive
+                string[] array2 = Directory.GetFiles(@""+text1+"", "*.log"); // <-- Case-insensitive
+                label4.Text = array2.Count().ToString();
+                listBox1.Items.Clear();
+                foreach (string s in array2)
+                {
+                    listBox1.Items.Add(s);
+                }
+            }
+            else
+            {
+                listBox1.Items.Clear();
+
+                string message = "Directory is not selected";
+                listBox1.Items.Add(message);
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex >= 0)
+            {
+                string dizi = "C:\\";
+                //string[] array1 = Directory.GetFiles(@"C:\");
+                //string[] array1 = Directory.GetFiles(dizi);
+                string[] array1 = Directory.GetFiles(@""+text1+"");
+                int count1 = array1.Count();
+                //label4.Text = count1.ToString();
+                //label4.Text = array1[1];
+                // listBox2.Items.Add(array1[0]);
+                listBox1.Items.Clear();
+                foreach (var item in array1)
+                {
+                    listBox1.Items.Add(item);
+                }
+            }
+            else
+            {
+                listBox1.Items.Clear();
+
+                string message = "Directory is not selected";
+                listBox1.Items.Add(message);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            string[] AltKlasorler = Directory.GetDirectories("C:\\");
+            foreach (var item in AltKlasorler)
+            {
+                listBox1.Items.Add(item);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            string[] AltKlasorler = Directory.GetDirectories("C:\\", "p*");
+            foreach (var item in AltKlasorler)
+            {
+                listBox1.Items.Add(item);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //label4.Text = Directory.GetDirectoryRoot("C:\\Dosya");
+            //label4.Text = Directory.GetDirectoryRoot(@""+text1+"");
+            label4.Text = Directory.GetDirectoryRoot(@"C:\Deneme1\temp");
+            Directory.GetDirectoryRoot(@"C:\Deneme1\temp");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //Directory.GetParent("C:\\Deneme1\\temp");
+            Directory.GetParent("C:\\Deneme1\\temp");
+
+            //System.IO.DirectoryInfo directoryInfo =
+            //        System.IO.Directory.GetParent(path);
+
+            //System.Console.WriteLine(directoryInfo.FullName);
+
+            //string path1 = "C:\\Deneme1\\temp";
+            string path1 = (@""+text1+"");
+            System.IO.DirectoryInfo directoryInfo =
+                   System.IO.Directory.GetParent(path1);
+            label4.Text = directoryInfo.Name;
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            //text1= listBox1.SelectedItem.ToString();
+            //label4.Text = text1;
+            //string[] array1 = Directory.GetFiles(@""+text1+"");
+            //foreach (var item in array1)
+            //{
+            //    listBox2.Items.Add(item);
+            //}
+            string directoryName;
+            //string path1 = (@""+text1+"");
+            string path1 = ("C:\\Deneme1\\temp\\new1");
+            directoryName = Path.GetDirectoryName(path1);
+            label4.Text = directoryName;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            string fileName = "haber.txt";
+            string path1 = @"new1";
+            string path2 = @"\new1";
+            string fullPath;
+
+            //fullPath = Path.GetFullPath(path1);
+            //Console.WriteLine("GetFullPath('{0}') returns '{1}'",
+            //    path1, fullPath);
+
+            fullPath = Path.GetFullPath(path1);
+            //label4.Text=fullPath;
+
+            //fullPath = Path.GetFullPath(fileName);
+            //Console.WriteLine("GetFullPath('{0}') returns '{1}'",
+            //    fileName, fullPath);
+
+            fullPath = Path.GetFullPath(fileName);
+            //label4.Text=fullPath;
+
+            //fullPath = Path.GetFullPath(path2);
+            //Console.WriteLine("GetFullPath('{0}') returns '{1}'",
+            //    path2, fullPath);
+
+            fullPath = Path.GetFullPath(path2);
+            label4.Text=fullPath; // C:\new1
+
+            // Output is based on your current directory, except
+            // in the last case, where it is based on the root drive
+            // GetFullPath('mydir') returns 'C:\temp\Demo\mydir'
+            // GetFullPath('myfile.ext') returns 'C:\temp\Demo\myfile.ext'
+            // GetFullPath('\mydir') returns 'C:\mydir'
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+            {
+            this.Text = "xxx";
+        }
+    }
+}
